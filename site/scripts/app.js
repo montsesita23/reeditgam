fd17d0960cb0e05b6e400d26d8800847931e1e88
Montserrat <montse.esquivelalbarran4@gmail.com>
2015-04-27 04:47:48 +0200

    0.4 Agregar nuevos posts estaticos

--- site/scripts/app.js
+++ site/scripts/app.js
@@ -1,38 +1,23 @@
 console.log ("#Montse: Cargo app.js");
-var modulo1=
+var modulo1 = 
 	angular.module("reeditgam",[]);
 modulo1.controller("mainCtrl",[
 	'$scope',
 	function($scope){
 		$scope.test = "Hola Angular";
+		// Modelo
 		$scope.posts = 
-	   [{title: "Post 1", upvotes: 5},
-		{title: "Post 2", upvotes: 15},
-		{title: "Post 3", upvotes: 7},
-		{title: "Post 4", upvotes: 9},
-		{title: "Post 5", upvotes: 3},
-		{title: "Post 6", upvotes: 5}];
-		//Metodo del controlador
+		[{title: "Post 1", upvotes: 5},
+		 {title: "Post 2", upvotes: 15},
+		 {title: "Post 3", upvotes: 7},
+		 {title: "Post 4", upvotes: 9},
+		 {title: "Post 5", upvotes: 3},
+		 {title: "Post 6", upvotes: 5}];
 
-		$scope.addPost = function(){
-			if(!$scope.title || $scope.title === "")
-			{
-              alert("NO se permite postear titulos vacios")
-              return;
-			}
-		$scope.posts.push(
-			{
-				title:$scope.title,
-				link:$scope.link,
-			 upvotes: 0
-			});
-           //Two-way data bindingit
-           $scope.title = "";
-            $scope.link = "";
-	    };	
-	    //metodo que incrementa
-	    // el voto de un post en una unidad
-	    $scope.incrementUpvotes = function(post){
-             post.upvotes += 1;
-	    }
+		 // Metodo del controlador
+		 $scope.addPost = function(){
+		 	$scope.posts.push(
+		 		{title:"Nuevo Post",
+		 		 upvotes: 0});
+		 };
 	}]);
