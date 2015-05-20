a87f5466aee3a4c3db4229379ff4f54c8d0edae9
Montserrat <montse.esquivelalbarran4@gmail.com>
2015-05-20 17:06:36 +0200

    10.1 Se agrega sweet alert para angular a las dependencias de Bower

--- site/scripts/app.js
+++ site/scripts/app.js
@@ -1,92 +1,88 @@
-console.log("#Montse: Cargo app.js");
-// Inyectadon el modulo de ui-router
-// como parametro del arreglo de objetos
-// del modulo
-var modulo1 = 
+console.log ("#Montse: Cargo app.js");
+//Inyectando el modulo ui-router
+//como parametro del arreglo de objetos
+//del modulo
+var modulo1=
 	angular.module("reeditgam",['ui.router']);
-
-// Configurando las rutas
-// Recibe un arreglo de elementos
+//configurando las rutas
+//recibe un arreglo de elementos
 modulo1.config(
-	['$stateProvider',
-	'$urlRouterProvider',
-	function($stateProvider, $urlRouterProvider){
-		// Iniciando rutina de configuracion
-		// Creando ruta /Home
-		$stateProvider.state('home',{
-			//Definiendo estado como un objeto
-			url:"/home", // Url que define el estado
-			templateUrl: "/home.html", // Plantilla base para el estado
-			controller: "mainCtrl"
-		});
-		// Creando Ruta de visualizacion
-		// de Post
-		$stateProvider.state('posts',{
-			url: "/posts/{id}",
-			templateUrl: "/posts.html",
-			controller: "postsCtrl"
-		});
-		// Url por defecto
-		$urlRouterProvider.otherwise('home');
-	}]);
-
-// Creando un servicio del tipo factory
-modulo1.factory('posts',[function(){
-	// Cuerpo del factory llamado post
-	var o = {
-		posts : [
-			{	
-				title: "post 1", upvotes: 15,
-				comments: [
-					{author: "Karina", body:"Esto esta de pelos.",
-					upvotes:3},
-					{author: "Gamaliel", body:"Esto es basura.",
-					upvotes:0}]
-			},
-			{	
-				title: "post 2", upvotes: 4,
-				comments: [
-					{author: "Coco", body:"Esto es asombroso.",
-					upvotes:5},
-					{author: "Cristian", body:"Esto esta aburrido.",
-					upvotes:1}]
-			}
-		]
-	};
-	// Retronado objeto de datos persistentes
-	return o;
+         ['$stateProvider',
+          '$urlRouterProvider',
+           function($stateProvider, $urlRouterProvider){
+//iniciando rutina de configuracion
+//creando ruta/home
+$stateProvider.state('home',{
+//definiendo estado como un objeto
+url:"/home", //Url que define el estado
+templateUrl: "/home.html", // Plantilla base para el estado
+controller: "mainCtrl"
+});
+//Creando Ruta de visualizacion
+//de Post
+$stateProvider.state('post',{
+url: "/posts/{id}",
+templateUrl: "/posts.html",
+controller: "postsCtrl"
+});
+//Url por defecto
+$urlRouterProvider.otherwise('home');
 }]);
-
+//creando un servicio del tipo factory
+modulo1.factory('posts',[function(){
+//cuerpo del factory llamado post
+var o = {
+post : [
+       {
+title: "post 1", upvotes: 15,
+comments: [
+{author: "Karina", body:"Esto esta de pelos.",
+upvotes:3},
+{author: "Gamaliel", body:"Esto es basura.",
+upvotes:0}]
+},
+{
+title:"post 2", upvotes: 4,
+comments: [
+{author: "Coco", body:"Esto es asombroso.",
+upvotes:5},
+{author: "Cristian", body:"Esto esta aburrido.",
+upvotes:1}]
+}
+]
+};
+// Retronado objeto de datos persistentes
+return o;
+}]);
 // Creando controlador	
 // dependency injection
 // Creando controlador mainCtrl
 modulo1.controller("mainCtrl",[
-	'$scope','posts', // Inyectando factory post
+	'$scope','posts', //inyectando factory post
 	function($scope, posts){
 		$scope.test = "Hola Angular";
-		
-		// Modelo al cual se le asigna
-		// el resultado del factory
-		$scope.posts = posts.posts;
+//modelo al cual se le asigna
+//el resultado del factory
+$scope.posts = posts.posts;
 
-		 // Metodo del controlador
-		 $scope.addPost = function(){
-		 	if(!$scope.title || $scope.title === "")
-		 	{
-		 		alert("No se permite postear titulos vacios");
-		 		return;
-		 	}
-		 	$scope.posts.push(
-		 		{
-		 			title: $scope.title,
-		 			link: $scope.link,
-		 		 	upvotes: 0
-		 		 });
-		 	// Two-way data binding
-		 	$scope.title = "";
-		 	$scope.link = "";
-		 };
-		 // Metodo que incrementa el voto
+//metodo del controlador
+$scope.addPosts = function(){
+if(!$scope.title || $scope.title === "")
+{
+alert("No se permite postear titulos vacios")
+return;
+}
+$scope.posts.push(
+{
+title: $scope.title,
+link: $scope.link,
+upvotes: 0
+});
+//Two-way data binding
+$scope.title = "";
+$scope.link = "";
+};
+// Metodo que incrementa el voto
 		 // de un post en una unidad
 		 $scope.incrementUpvotes = function(post){
 		 	post.upvotes += 1;
@@ -101,3 +97,6 @@ modulo1.controller("postCtrl",[
 		// Cuerpo del controlador
 		
 	});
+
+
+
